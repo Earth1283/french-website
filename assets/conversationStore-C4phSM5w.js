a384ee96db1ee00b1de44de22a2c9fa2528abd0d
Earth1283 <Earth1283@users.noreply.github.com>
2026-09-19 14:15:55 +0000

    deploy: 0bc99a9f7530037938a1ed2dbf98f5c6584dcded

---
+++ assets/conversationStore-C4phSM5w.js
@@ -0,0 +1,1 @@
+import{Et as e,Tt as t}from"./ui-CepEm5pz.js";var n=e()(t(e=>({geminiApiKey:``,difficulty:1,setApiKey:t=>e({geminiApiKey:t}),setDifficulty:t=>e({difficulty:t})}),{name:`french-conversation`}));export{n as t};
